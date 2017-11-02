293cdfc92de349a6cd2bf2aa51b3b1abec0811eb
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-02 19:23:01 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,6 +1,14 @@
 module Dance
   def twirl
-    "Im twirling!"
+    "I'm twirling!"
+  end
+
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
   end
 
 end
